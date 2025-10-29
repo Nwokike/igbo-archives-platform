9e0856efe08bdbe87fe3f7f84faf4f441ed6ccff
Onyeka Nwokike <nwokikeonyeka@gmail.com>
2025-10-29 21:27:54 +0000

    Guest CAPTCHA finally fixed

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+    
+    def ready(self):
+        # Simple fix: Force django_comments to use our custom form
+        import django_comments
+        from core.forms import CaptchaThreadedCommentForm
+        django_comments.get_form = lambda: CaptchaThreadedCommentForm
